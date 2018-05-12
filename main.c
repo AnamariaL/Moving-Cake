210038b0fb8febe40ad21d7c32f50693691ca939
AnamariaL <32013978+AnamariaL@users.noreply.github.com>
2018-05-12 22:07:32 +0300

    Update main.c

--- main.c
+++ main.c
@@ -2,32 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-int hanoi(int n,char a[],char b[],char c[]){
-
+int moveLayer(int n,char source,char destination,char spare){
 //set selection to n (i.e) if n is 1
     if(n==1){
-        printf("\nMove layer %d from  %c plate to  %c plate",n,a,b);
+        printf("\nMove layer %d from  %c plate to  %c plate",n,source,destination);
     }
     else{
-    //call the function hanoi(recursion) and return value to the function parameters
+    //call the function moveLayer(recursion) and return value to the function parameters
     //(n will get replaced with value of n-1).
-   // Therefore  n-1 over n,a over a,c over b,b over c
-        hanoi (n-1,a,c,b);
-        printf("\nMove layer %d from  %c plate to  %c plate",n,a,b);
-        //call the function hanoi and like the above function return values to function hanoi
-        //as n-1 for n, c for a, b for b,a for c
-        hanoi (n-1,c,b,a);
+   // Therefore  n-1 over n,source over source,spare over destination,destination over spare
+        moveLayer(n-1,source,spare,destination);
+        printf("\nMove layer %d from  %c plate to  %c plate",n,source,destination);
+        //call the function moveLayer and like the above function return values to function moveLayer
+        //as n-1 for n, spare for source, destination for destination,source for spare
+         moveLayer(n-1,spare,destination,source);
     }
 }
 int main() {
-int n;//layers
-char a[6]="silver"; //declare array to silver
-char b[6]="bronze";//declare array to bronze
-char c[6]="golden";//declare array to golden
-printf("How many layers of cake  : ");
-scanf("%d",&n);
+    int n;//layers
+    printf("How many layers of cake  : ");
+    scanf("%d",&n);
 //call the function and return the values
-hanoi (n,a,b,c);
-
+    moveLayer(n,'S','G','B');
     return 0;
 }
