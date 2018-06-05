4edfc66ae97c5f86dcbeb1fbc0f9d52120525c2c
AnamariaL <32013978+AnamariaL@users.noreply.github.com>
2018-06-05 14:59:06 +0300

    Update Moving-Cake.py

--- Moving-Cake.py
+++ Moving-Cake.py
@@ -3,14 +3,14 @@ def CakeLayers(n , source="Silver", dest="Golden", temp="Bronze"):
 
     if n == 1:
     #moves final remaining layer from source to dest
-        print ("Move disk 1 from rod",source,"to rod",dest)
+        print ("Move layer 1 from rod",source,"to rod",dest)
         return 
         
     #moves layer from source to temp using dest
     CakeLayers(n-1, source, temp, dest)
     
     #moves layer from source to dest directly
-    print ("Move disk",n,"from rod",source,"to rod",dest)
+    print ("Move layer",n,"from rod",source,"to rod",dest)
     
     #moves layer from temp to dest using source
     CakeLayers(n-1, temp, dest, source)
